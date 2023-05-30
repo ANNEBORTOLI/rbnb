1c95aedc293cb9e7535320d42675ad26a4daef8f
Anne Bortoli <annebortoli@gmail.com>
2023-05-30 16:38:29 -0300

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -29,8 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_201833) do
   create_table "reservations", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
-    t.float "price"
-    t.float "total_price"
     t.bigint "user_id", null: false
     t.bigint "property_id", null: false
     t.datetime "created_at", null: false
