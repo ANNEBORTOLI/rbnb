9320fe424227655b438ef76451056889a38cf54e
Anne Bortoli <annebortoli@gmail.com>
2023-05-31 12:21:53 -0300

    index without login

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,4 +1,5 @@
 class PropertiesController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[home index show]
   before_action :set_property, only: %i[show edit update destroy]
 
   def index
