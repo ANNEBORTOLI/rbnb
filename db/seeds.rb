872297aa002ce8e89f27a4dd7d2e06fa54803e85
Anne Bortoli <annebortoli@gmail.com>
2023-06-01 17:03:34 -0300

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,10 +47,8 @@ Property.create!(
   total_occupancy: rand(1..10),
   address: "Copacabana, Rio de Janeiro",
   title: "Imovel luxuoso com vista para o mar",
-  price: rand(50..500),
-  summary: "Regras do condomínio: não é permitido aparelhos de som em nenhuma das áreas comuns do condomínio; não é permitido copos ou garrafas de vidro na área de piscina, não é permitido que pets andem sem coleira na área comum;
-  Nao e permitido pets na area da piscina. Sempre recolher o coco dos pets .
-  ",
+  price: 199,
+  summary: "Enjoy the sea and mountain views from the pool, lush garden and private patio leading off the living area Beautiful French Provencal stone cottage set in private indigenous garden overlooking Llandudno beach. The cottage was lovingly built with exceptional attention to detail, including stone floors, high beamed ceilings, antique light fittings, French wrought iron work and wooden shutters.",
   image_url: "https://s2.glbimg.com/T7ZAimBgK10jaelSvIABQ9fBDLk=/smart/e.glbimg.com/og/ed/f/original/2019/12/10/apartamentos-design-airbnb1.jpg"
 )
 
@@ -60,10 +58,8 @@ Property.create!(
   total_occupancy: rand(1..10),
   address: "Ipanema, Rio de Janeiro",
   title: "Apartamento maravilhoso em Ipa",
-  price: rand(50..500),
-  summary: "Regras do condomínio: não é permitido aparelhos de som em nenhuma das áreas comuns do condomínio; não é permitido copos ou garrafas de vidro na área de piscina, não é permitido que pets andem sem coleira na área comum;
-  Nao e permitido pets na area da piscina. Sempre recolher o coco dos pets .
-  ",
+  price: 299,
+  summary: "Enjoy the sea and mountain views from the pool, lush garden and private patio leading off the living area Beautiful French Provencal stone cottage set in private indigenous garden overlooking Llandudno beach. The cottage was lovingly built with exceptional attention to detail, including stone floors, high beamed ceilings, antique light fittings, French wrought iron work and wooden shutters.",
   image_url: "https://www.blogvambora.com.br/wp-content/uploads/2021/02/Apto-Sacada-Vila-Olimpia.jpg"
 )
 
@@ -73,10 +69,8 @@ Property.create!(
   total_occupancy: rand(1..10),
   address: "Lagoa, Rio de Janeiro",
   title: "Cobertura na Lagoa Rodrigo de Freitas",
-  price: rand(50..500),
-  summary: "Regras do condomínio: não é permitido aparelhos de som em nenhuma das áreas comuns do condomínio; não é permitido copos ou garrafas de vidro na área de piscina, não é permitido que pets andem sem coleira na área comum;
-  Nao e permitido pets na area da piscina. Sempre recolher o coco dos pets .
-  ",
+  price: 399,
+  summary: "Enjoy the sea and mountain views from the pool, lush garden and private patio leading off the living area Beautiful French Provencal stone cottage set in private indigenous garden overlooking Llandudno beach. The cottage was lovingly built with exceptional attention to detail, including stone floors, high beamed ceilings, antique light fittings, French wrought iron work and wooden shutters.",
   image_url: "http://hus.arq.br/app/uploads/2018/05/9-9-1280x720.png"
 )
 
@@ -86,10 +80,8 @@ Property.create!(
   total_occupancy: rand(1..10),
   address: "Jardim Botânico, Rio de Janeiro",
   title: "Jardim Botânico, pertinho do Parque Lage",
-  price: rand(50..500),
-  summary: "Regras do condomínio: não é permitido aparelhos de som em nenhuma das áreas comuns do condomínio; não é permitido copos ou garrafas de vidro na área de piscina, não é permitido que pets andem sem coleira na área comum;
-  Nao e permitido pets na area da piscina. Sempre recolher o coco dos pets .
-  ",
+  price: 345,
+  summary: "Enjoy the sea and mountain views from the pool, lush garden and private patio leading off the living area Beautiful French Provencal stone cottage set in private indigenous garden overlooking Llandudno beach. The cottage was lovingly built with exceptional attention to detail, including stone floors, high beamed ceilings, antique light fittings, French wrought iron work and wooden shutters.",
   image_url: "https://news.airbnb.com/wp-content/uploads/sites/4/2018/08/mostwishlisted-1-1.jpg"
 )
 
@@ -99,10 +91,8 @@ Property.create!(
   total_occupancy: rand(1..10),
   address: "Peixoto, Rio de Janeiro",
   title: "Linda propriedade no bairro Peixoto em Copacabana",
-  price: rand(50..500),
-  summary: "Regras do condomínio: não é permitido aparelhos de som em nenhuma das áreas comuns do condomínio; não é permitido copos ou garrafas de vidro na área de piscina, não é permitido que pets andem sem coleira na área comum;
-  Nao e permitido pets na area da piscina. Sempre recolher o coco dos pets .
-  ",
+  price: 238,
+  summary: "Enjoy the sea and mountain views from the pool, lush garden and private patio leading off the living area Beautiful French Provencal stone cottage set in private indigenous garden overlooking Llandudno beach. The cottage was lovingly built with exceptional attention to detail, including stone floors, high beamed ceilings, antique light fittings, French wrought iron work and wooden shutters.",
   image_url: "https://news.airbnb.com/wp-content/uploads/sites/4/2018/08/MW10-101.jpg"
 )
 
@@ -112,10 +102,8 @@ Property.create!(
   total_occupancy: rand(1..10),
   address: "Barra da Tijuca, Rio de Janeiro",
   title: "Fique perto da praia mais gostosa do Rio!",
-  price: rand(50..500),
-  summary: "Regras do condomínio: não é permitido aparelhos de som em nenhuma das áreas comuns do condomínio; não é permitido copos ou garrafas de vidro na área de piscina, não é permitido que pets andem sem coleira na área comum;
-  Nao e permitido pets na area da piscina. Sempre recolher o coco dos pets .
-  ",
+  price: 219,
+  summary: "Enjoy the sea and mountain views from the pool, lush garden and private patio leading off the living area Beautiful French Provencal stone cottage set in private indigenous garden overlooking Llandudno beach. The cottage was lovingly built with exceptional attention to detail, including stone floors, high beamed ceilings, antique light fittings, French wrought iron work and wooden shutters.",
   image_url: "https://news.airbnb.com/wp-content/uploads/sites/4/2018/08/MW8-8-2.jpg"
 )
 
@@ -125,10 +113,8 @@ Property.create!(
   total_occupancy: rand(1..10),
   address: "Leme, Rio de Janeiro",
   title: "Apartamento aconchegante no Leme",
-  price: rand(50..500),
-  summary: "Regras do condomínio: não é permitido aparelhos de som em nenhuma das áreas comuns do condomínio; não é permitido copos ou garrafas de vidro na área de piscina, não é permitido que pets andem sem coleira na área comum;
-  Nao e permitido pets na area da piscina. Sempre recolher o coco dos pets .
-  ",
+  price: 234,
+  summary: "Enjoy the sea and mountain views from the pool, lush garden and private patio leading off the living area Beautiful French Provencal stone cottage set in private indigenous garden overlooking Llandudno beach. The cottage was lovingly built with exceptional attention to detail, including stone floors, high beamed ceilings, antique light fittings, French wrought iron work and wooden shutters.",
   image_url: "https://news.airbnb.com/wp-content/uploads/sites/4/2018/08/MW7-7-2.jpg"
 )
 
@@ -138,10 +124,8 @@ Property.create!(
   total_occupancy: rand(1..10),
   address: "São Conrado, Rio de Janeiro",
   title: "Apartamento em São Conrado",
-  price: rand(50..500),
-  summary: "Regras do condomínio: não é permitido aparelhos de som em nenhuma das áreas comuns do condomínio; não é permitido copos ou garrafas de vidro na área de piscina, não é permitido que pets andem sem coleira na área comum;
-  Nao e permitido pets na area da piscina. Sempre recolher o coco dos pets .
-  ",
+  price: 250,
+  summary: "Enjoy the sea and mountain views from the pool, lush garden and private patio leading off the living area Beautiful French Provencal stone cottage set in private indigenous garden overlooking Llandudno beach. The cottage was lovingly built with exceptional attention to detail, including stone floors, high beamed ceilings, antique light fittings, French wrought iron work and wooden shutters.",
   image_url: "https://news.airbnb.com/wp-content/uploads/sites/4/2018/08/MW6-6-2.jpg"
 )
 
@@ -151,10 +135,8 @@ Property.create!(
   total_occupancy: rand(1..10),
   address: "Botafogo, Rio de Janeiro",
   title: "Apartamento funcional em Botafogo",
-  price: rand(50..500),
-  summary: "Regras do condomínio: não é permitido aparelhos de som em nenhuma das áreas comuns do condomínio; não é permitido copos ou garrafas de vidro na área de piscina, não é permitido que pets andem sem coleira na área comum;
-  Nao e permitido pets na area da piscina. Sempre recolher o coco dos pets .
-  ",
+  price: 170,
+  summary: "Enjoy the sea and mountain views from the pool, lush garden and private patio leading off the living area Beautiful French Provencal stone cottage set in private indigenous garden overlooking Llandudno beach. The cottage was lovingly built with exceptional attention to detail, including stone floors, high beamed ceilings, antique light fittings, French wrought iron work and wooden shutters.",
   image_url: "https://news.airbnb.com/wp-content/uploads/sites/4/2018/08/most-wishlisted-2-2-21.jpg"
 )
 
@@ -164,10 +146,8 @@ Property.create!(
   total_occupancy: rand(1..10),
   address: "Catete, Rio de Janeiro",
   title: "Flat, próximo ao metrô",
-  price: rand(50..500),
-  summary: "Regras do condomínio: não é permitido aparelhos de som em nenhuma das áreas comuns do condomínio; não é permitido copos ou garrafas de vidro na área de piscina, não é permitido que pets andem sem coleira na área comum;
-  Nao e permitido pets na area da piscina. Sempre recolher o coco dos pets .
-  ",
+  price: 195,
+  summary: "Enjoy the sea and mountain views from the pool, lush garden and private patio leading off the living area Beautiful French Provencal stone cottage set in private indigenous garden overlooking Llandudno beach. The cottage was lovingly built with exceptional attention to detail, including stone floors, high beamed ceilings, antique light fittings, French wrought iron work and wooden shutters.",
   image_url: "https://news.airbnb.com/wp-content/uploads/sites/4/2018/08/MW9-9-2.jpg"
 )
 
@@ -177,10 +157,8 @@ Property.create!(
   total_occupancy: rand(1..10),
   address: "Flamengo, Rio de Janeiro",
   title: "Cobertura duplex no Flamengo",
-  price: rand(50..500),
-  summary: "Regras do condomínio: não é permitido aparelhos de som em nenhuma das áreas comuns do condomínio; não é permitido copos ou garrafas de vidro na área de piscina, não é permitido que pets andem sem coleira na área comum;
-  Nao e permitido pets na area da piscina. Sempre recolher o coco dos pets .
-  ",
+  price: 179,
+  summary: "Enjoy the sea and mountain views from the pool, lush garden and private patio leading off the living area Beautiful French Provencal stone cottage set in private indigenous garden overlooking Llandudno beach. The cottage was lovingly built with exceptional attention to detail, including stone floors, high beamed ceilings, antique light fittings, French wrought iron work and wooden shutters.",
   image_url: "https://news.airbnb.com/wp-content/uploads/sites/4/2018/08/MW3-3.jpg"
 )
 
@@ -190,10 +168,8 @@ Property.create!(
   total_occupancy: rand(1..10),
   address: "Largo do Machado, Rio de Janeiro",
   title: "Apartamento no melhor ponto do Largo do Machado",
-  price: rand(50..500),
-  summary: "Regras do condomínio: não é permitido aparelhos de som em nenhuma das áreas comuns do condomínio; não é permitido copos ou garrafas de vidro na área de piscina, não é permitido que pets andem sem coleira na área comum;
-  Nao e permitido pets na area da piscina. Sempre recolher o coco dos pets .
-  ",
+  price: 139,
+  summary: "Enjoy the sea and mountain views from the pool, lush garden and private patio leading off the living area Beautiful French Provencal stone cottage set in private indigenous garden overlooking Llandudno beach. The cottage was lovingly built with exceptional attention to detail, including stone floors, high beamed ceilings, antique light fittings, French wrought iron work and wooden shutters.",
   image_url: "https://news.airbnb.com/wp-content/uploads/sites/4/2018/08/MW5-5-21.jpg"
 )
 
@@ -203,10 +179,8 @@ Property.create!(
   total_occupancy: rand(1..10),
   address: "Recreio dos Bandeirantes, Rio de Janeiro",
   title: "Apartamento no Recreio dos Bandeirantes",
-  price: rand(50..500),
-  summary: "Regras do condomínio: não é permitido aparelhos de som em nenhuma das áreas comuns do condomínio; não é permitido copos ou garrafas de vidro na área de piscina, não é permitido que pets andem sem coleira na área comum;
-  Nao e permitido pets na area da piscina. Sempre recolher o coco dos pets .
-  ",
+  price: 119,
+  summary: "Enjoy the sea and mountain views from the pool, lush garden and private patio leading off the living area Beautiful French Provencal stone cottage set in private indigenous garden overlooking Llandudno beach. The cottage was lovingly built with exceptional attention to detail, including stone floors, high beamed ceilings, antique light fittings, French wrought iron work and wooden shutters.",
   image_url: "https://news.airbnb.com/wp-content/uploads/sites/4/2018/08/MW4-41.jpg"
 )
 
